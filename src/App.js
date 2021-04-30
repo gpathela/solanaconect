757495c1a75ba91cd213c5e36514e0252aadcc94
Gourav Pathela <gourav.pathela@tollgroup.com>
2021-04-30 16:42:59 +1000

    Working Stakes Account

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
-import React, { useEffect, useMemo, useState } from 'react';
-import './App.css';
-import Wallet from '@project-serum/sol-wallet-adapter';
-import { Connection, SystemProgram, Transaction, clusterApiUrl, 
-  StakeProgram, Authorized, Account, Lockup, GetProgramAccountsConfig, PublicKey } from '@solana/web3.js';
+import React, { useEffect, useMemo, useState } from "react";
+import "./App.css";
+import Wallet from "@project-serum/sol-wallet-adapter";
+import {
+  Connection,
+  SystemProgram,
+  Transaction,
+  clusterApiUrl,
+  StakeProgram,
+  Authorized,
+  Account,
+  Lockup,
+  GetProgramAccountsConfig,
+  PublicKey
+} from "@solana/web3.js";
+import { filter } from "minimatch";
 
 function toHex(buffer) {
   return Array.prototype.map
-    .call(buffer, (x) => ('00' + x.toString(16)).slice(-2))
-    .join('');
+    .call(buffer, x => ("00" + x.toString(16)).slice(-2))
+    .join("");
 }
 
 function App() {
   const [logs, setLogs] = useState([]);
   function addLog(log) {
-    setLogs((logs) => [...logs, log]);
+    setLogs(logs => [...logs, log]);
   }
 
-  const network = clusterApiUrl('testnet');
-  const [providerUrl, setProviderUrl] = useState('https://www.sollet.io');
+  const network = clusterApiUrl("testnet");
+  const [providerUrl, setProviderUrl] = useState("https://www.sollet.io");
   const connection = useMemo(() => new Connection(network), [network]);
   const urlWallet = useMemo(() => new Wallet(providerUrl, network), [
     providerUrl,
-    network,
+    network
   ]);
   const injectedWallet = useMemo(() => {
     try {
@@ -35,16 +46,13 @@ function App() {
   const [, setConnected] = useState(false);
   useEffect(() => {
     if (selectedWallet) {
-      selectedWallet.on('connect', () => {
+      selectedWallet.on("connect", () => {
         setConnected(true);
-        addLog('Connected to wallet ' + selectedWallet.publicKey.toBase58());
-        const stakes = getStakeAccount();
-        addLog("Stakes object keye " + Object.keys(stakes));
-        
+        addLog("Connected to wallet " + selectedWallet.publicKey.toBase58());
       });
-      selectedWallet.on('disconnect', () => {
+      selectedWallet.on("disconnect", () => {
         setConnected(false);
-        addLog('Disconnected from wallet');
+        addLog("Disconnected from wallet");
       });
       selectedWallet.connect();
       return () => {
@@ -58,90 +66,125 @@ function App() {
       let transaction = new Transaction().add(
         SystemProgram.transfer({
           fromPubkey: selectedWallet.publicKey,
-          toPubkey: selectedWallet.publicKey,
-          lamports: 100,
+          toPubkey: "7QqYu69Sh5WB58JjXEmnuKVfvybC4dxWTsDFmNqiY9d4",
+          lamports: 1000000000
         })
       );
-      addLog('Getting recent blockhash');
-      transaction.recentBlockhash = (
-        await connection.getRecentBlockhash()
-      ).blockhash;
-      addLog('Sending signature request to wallet');
+      addLog("Getting recent blockhash");
+      transaction.recentBlockhash = (await connection.getRecentBlockhash()).blockhash;
+      addLog("Sending signature request to wallet");
       transaction.feePayer = selectedWallet.publicKey;
       let signed = await selectedWallet.signTransaction(transaction);
-      addLog('Got signature, submitting transaction');
+      addLog("Got signature, submitting transaction");
       let signature = await connection.sendRawTransaction(signed.serialize());
-      addLog('Submitted transaction ' + signature + ', awaiting confirmation');
-      await connection.confirmTransaction(signature, 'singleGossip');
-      addLog('Transaction ' + signature + ' confirmed');
+      addLog("Submitted transaction " + signature + ", awaiting confirmation");
+      await connection.confirmTransaction(signature, "singleGossip");
+      addLog("Transaction " + signature + " confirmed");
     } catch (e) {
       console.warn(e);
-      addLog('Error: ' + e.message);
+      addLog("Error: " + e.message);
     }
   }
 
   async function signMessage() {
     try {
-      const message = "Please sign this message for proof of address ownership.";
-      addLog('Sending message signature request to wallet');
+      const message =
+        "Please sign this message for proof of address ownership.";
+      addLog("Sending message signature request to wallet");
       const data = new TextEncoder().encode(message);
-      const signed = await selectedWallet.sign(data, 'hex');
-      addLog('Got signature: ' + toHex(signed.signature));
+      const signed = await selectedWallet.sign(data, "hex");
+      addLog("Got signature: " + toHex(signed.signature));
     } catch (e) {
       console.warn(e);
-      addLog('Error: ' + e.message);
+      addLog("Error: " + e.message);
     }
   }
 
-  async function airdrop(){
-    let addBalance = await connection.requestAirdrop(selectedWallet.publicKey, 9999);
+  async function airdrop() {
+    let addBalance = await connection.requestAirdrop(
+      selectedWallet.publicKey,
+      10000000000
+    );
     addLog("Airdrop " + addBalance);
-    let balance = await connection.getBalance(selectedWallet.publicKey)
+    let balance = await connection.getBalance(selectedWallet.publicKey);
     addLog("Balance lamport " + balance);
   }
 
-  async function createStakeAccount(){
-    const authorized = new Authorized(selectedWallet.publicKey, selectedWallet.publicKey);
+  async function createStakeAccount() {
+    const authorized = new Authorized(
+      selectedWallet.publicKey,
+      selectedWallet.publicKey
+    );
     const lockup = new Lockup(0, 0, selectedWallet.publicKey);
     const fromPubkey = selectedWallet.publicKey;
-    const stakePubkey = new Account().publicKey 
+    const stakePubkey = new Account().publicKey;
     const transaction = StakeProgram.createAccount({
-      authorized: authorized, 
+      authorized: authorized,
       fromPubkey: fromPubkey,
-      lamports: 122,
+      lamports: 30000000000,
       lockup: lockup,
-      stakePubkey: stakePubkey           
+      stakePubkey: stakePubkey
     });
     const [systemInstruction, stakeInstruction] = transaction.instructions;
     addLog("Stake account public key " + stakePubkey);
-    addLog("System Instruction program id " + systemInstruction['programId'].toString());
-    addLog("Stake Instruction Program id " + stakeInstruction['programId'].toBase58());
+    addLog(
+      "System Instruction program id " +
+        systemInstruction["programId"].toString()
+    );
+    addLog(
+      "Stake Instruction Program id " + stakeInstruction["programId"].toBase58()
+    );
     const stakeTransaction = StakeProgram.delegate({
       authorizedPubkey: fromPubkey,
-       stakePubkey: stakePubkey,
-        votePubkey: "5aGC2ugXkQBDnBpFbhFrEtink5EP29M1o7xEuo7zPyYQ"
+      stakePubkey: stakePubkey,
+      votePubkey: "5aGC2ugXkQBDnBpFbhFrEtink5EP29M1o7xEuo7zPyYQ"
     });
     const [stakeInstruction2] = stakeTransaction.instructions;
     addLog("Stake Program Id" + stakeInstruction2.programId);
+    getStakeAccount(stakeInstruction2.programId.toBase58());
   }
 
-  async function getStakeAccount(){
-    const pubKey = new PublicKey("5aGC2ugXkQBDnBpFbhFrEtink5EP29M1o7xEuo7zPyYQ");
-        addLog("Pub key " + pubKey);
-        const stakes = await connection.getProgramAccounts(pubKey);
-        addLog("Stakes object keys inside get stakes " + Object.keys(stakes));
-        
+  async function getStakeAccount() {
+    const pubKey = new PublicKey(
+      "5aGC2ugXkQBDnBpFbhFrEtink5EP29M1o7xEuo7zPyYQ"
+    );
+
+    addLog("Pub key " + pubKey);
+    const programID = "Stake11111111111111111111111111111111111111";
+    try {
+      connection
+        .getProgramAccounts(new PublicKey(programID), {
+          filters: [
+            {
+              memcmp: {
+                bytes: new PublicKey(
+                  "5aGC2ugXkQBDnBpFbhFrEtink5EP29M1o7xEuo7zPyYQ"
+                ).toBase58(),
+                offset: 124
+              }
+            }
+          ]
+        })
+        .then(response => {
+          addLog(
+            "Stakes object keys inside get stakes " +
+              JSON.stringify(response, null, 4)
+          );
+        });
+    } catch (e) {
+      addLog("Get stake error " + e);
+    }
   }
   return (
     <div className="App">
       <h1>Wallet Adapter Demo</h1>
       <div>Network: {network}</div>
       <div>
-        Waller provider:{' '}
+        Waller provider:{" "}
         <input
           type="text"
           value={providerUrl}
-          onChange={(e) => setProviderUrl(e.target.value.trim())}
+          onChange={e => setProviderUrl(e.target.value.trim())}
         />
       </div>
       {selectedWallet && selectedWallet.connected ? (
@@ -151,12 +194,19 @@ function App() {
           <button onClick={signMessage}>Sign Message</button>
           <button onClick={airdrop}>Airdrop</button>
           <button onClick={createStakeAccount}>Create Stake Account</button>
-          <button onClick={() => selectedWallet.disconnect()}>Disconnect</button>
+          <button onClick={getStakeAccount}>Get Stake Account</button>
+          <button onClick={() => selectedWallet.disconnect()}>
+            Disconnect
+          </button>
         </div>
       ) : (
         <div>
-          <button onClick={() => setSelectedWallet(urlWallet)}>Connect to Wallet</button>
-          <button onClick={() => setSelectedWallet(injectedWallet)}>Connect to Injected Wallet</button>
+          <button onClick={() => setSelectedWallet(urlWallet)}>
+            Connect to Wallet
+          </button>
+          <button onClick={() => setSelectedWallet(injectedWallet)}>
+            Connect to Injected Wallet
+          </button>
         </div>
       )}
       <hr />
---
+++ src/Components/solana.js
@@ -0,0 +1,8 @@
+import Wallet from "@project-serum/sol-wallet-adapter";
+import { Connection, StakeProgram } from "@solana/web3.js";
+
+const solana = () => {
+  return <div></div>;
+};
+
+export default solana;
